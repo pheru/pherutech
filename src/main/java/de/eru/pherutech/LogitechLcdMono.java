dd5be94a0b4d7d1d026bc4a2db79388622823ab4
pheru <Philipp.Bruckner@freenet.de>
2015-01-24 18:48:18 +0100

    LogitechLcdMono um weitere Methoden erweitert

--- src/main/java/de/eru/pherutech/LogitechLcdMono.java
+++ src/main/java/de/eru/pherutech/LogitechLcdMono.java
@@ -9,26 +9,52 @@ import de.eru.pherutech.exceptions.PixelsOutOfLcdSizeException;
  */
 public class LogitechLcdMono {
 
-    private native boolean startApplet();
-    private native boolean stopApplet();
+    public static final int LCD_PIXEL_SIZE = 160 * 43;
 
-    private native void paintPixels(byte[] pixels);
+    public enum LineNumber {
+        ONE, TWO, THREE, FOUR
+    }
 
-    public void start() throws NoKeyboardConnectedException {
+    private static native boolean supportsApplet();
+    private static native boolean startApplet();
+    private static native boolean stopApplet();
+    private static native void paintPixels(byte[] pixels);
+    private static native void writeLine(int lineNumber, String text);
+
+    private static String libraryPath = "LogitechLCD.dll"; //TODO mit ins Jar packen?
+    
+    public static boolean isSupported() {
+        return supportsApplet();
+    }
+
+    public static void start() throws NoKeyboardConnectedException, UnsatisfiedLinkError { //TODO falls im Jar, ULE unnötig
+        System.load(libraryPath);
         if (!startApplet()) {
-            throw new NoKeyboardConnectedException("TODO");//TODO
+            throw new NoKeyboardConnectedException("No Logitech-Keyboard connected!");
         }
     }
 
-    public void paint(byte[] pixels) throws PixelsOutOfLcdSizeException {
-        if (pixels.length <= 160 * 43) {
+    public static boolean stop() { //TODO kann stoppen schiefgehen?
+        return stopApplet();
+    }
+
+    public static void paint(byte[] pixels) throws PixelsOutOfLcdSizeException {
+        if (pixels.length <= LCD_PIXEL_SIZE) {
             paintPixels(pixels);
         } else {
-            throw new PixelsOutOfLcdSizeException("Zu viele Pixel!!11elf");
+            throw new PixelsOutOfLcdSizeException("Pixel-Array out of size! Maximum-size: " + LCD_PIXEL_SIZE + ".");
         }
     }
 
-    public boolean stop() {
-        return stopApplet();
+    public static void write(LineNumber lineNumber, String text) {
+        writeLine(lineNumber.ordinal() + 1, text);
+    }
+
+    public static String getLibraryPath() {
+        return libraryPath;
+    }
+
+    public static void setLibraryPath(String libraryPath) {
+        LogitechLcdMono.libraryPath = libraryPath;
     }
 }
